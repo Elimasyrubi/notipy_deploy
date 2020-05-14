2306d0e34898717c2550f37b5f317095fcc64a25
Eliezer Mas y rubi <elimasyrubi@gmail.com>
2020-05-14 10:36:53 -0300

    Updates

---
+++ precache-manifest.a5a044991d1601f64db6fc3323b671d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a36f7e1c12ff1b090ae34e66259378",
+    "url": "/notipy_deploy/index.html"
+  },
+  {
+    "revision": "5c773982f54992e168a7",
+    "url": "/notipy_deploy/static/css/main.052f1ff5.chunk.css"
+  },
+  {
+    "revision": "9a733c572c9b37103638",
+    "url": "/notipy_deploy/static/js/2.b4b80e70.chunk.js"
+  },
+  {
+    "revision": "73138c634db8d37252cbabcb33689813",
+    "url": "/notipy_deploy/static/js/2.b4b80e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c773982f54992e168a7",
+    "url": "/notipy_deploy/static/js/main.1933a878.chunk.js"
+  },
+  {
+    "revision": "0356ebe46d70636cfafe",
+    "url": "/notipy_deploy/static/js/runtime-main.120c3067.js"
+  }
+]);
